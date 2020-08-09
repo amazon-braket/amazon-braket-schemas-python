0214d5d8e15e0714f79c3a6bc3113ef89766d31c
🐅ⓚ <vundela@amazon.com>
2020-08-09 16:51:25 -0700

    Adding provider for rigetti/ionq

--- src/braket/device_schema/rigetti/rigetti_device_capabilities_v1.py
+++ src/braket/device_schema/rigetti/rigetti_device_capabilities_v1.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from typing import Dict
+from typing import Dict, Optional
 
 from pydantic import Field
 
@@ -21,6 +21,7 @@ from braket.device_schema.gate_model_qpu_paradigm_properties_v1 import (
     GateModelQpuParadigmProperties,
 )
 from braket.device_schema.jaqcd_device_action_properties import JaqcdDeviceActionProperties
+from braket.device_schema.rigetti.rigetti_provider_properties_v1 import RigettiProviderProperties
 from braket.schema_common import BraketSchemaBase, BraketSchemaHeader
 
 
@@ -101,3 +102,4 @@ class RigettiDeviceCapabilities(BraketSchemaBase, DeviceCapabilities):
     braketSchemaHeader: BraketSchemaHeader = Field(default=_PROGRAM_HEADER, const=_PROGRAM_HEADER)
     action: Dict[DeviceActionType, JaqcdDeviceActionProperties]
     paradigm: GateModelQpuParadigmProperties
+    provider: Optional[RigettiProviderProperties]
