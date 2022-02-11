93998ef54784e0509131c8ee24b31466004a4a7c
Kshitij Chhabra <kshitijc@users.noreply.github.com>
2022-02-11 10:49:04 -0800

    feat: Add OQC executedProgram in task result metadata (#20)
    * feat: Add OQC executedProgram in task result metadata
    
    * Revert reformatting

--- src/braket/task_result/additional_metadata.py
+++ src/braket/task_result/additional_metadata.py
@@ -18,6 +18,7 @@ from pydantic import BaseModel
 from braket.ir.annealing import Problem
 from braket.ir.jaqcd import Program
 from braket.task_result.dwave_metadata_v1 import DwaveMetadata
+from braket.task_result.oqc_metadata_v1 import OqcMetadata
 from braket.task_result.rigetti_metadata_v1 import RigettiMetadata
 from braket.task_result.simulator_metadata_v1 import SimulatorMetadata
 
@@ -39,4 +40,5 @@ class AdditionalMetadata(BaseModel):
     action: Union[Program, Problem]
     dwaveMetadata: Optional[DwaveMetadata]
     rigettiMetadata: Optional[RigettiMetadata]
+    oqcMetadata: Optional[OqcMetadata]
     simulatorMetadata: Optional[SimulatorMetadata]
