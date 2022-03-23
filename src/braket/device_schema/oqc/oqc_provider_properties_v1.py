b79c33c52edb575f553452270b1491b33b5e6aba
Matt Cordoba <matcor@amazon.com>
2022-03-23 14:41:33 -0700

    fix: Update OQC json schema to match GetDevice API call

--- src/braket/device_schema/oqc/oqc_provider_properties_v1.py
+++ src/braket/device_schema/oqc/oqc_provider_properties_v1.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from typing import Dict, List, TypeVar, Union
+from typing import Dict, TypeVar, Union
 
 from pydantic import Field
 
@@ -20,11 +20,8 @@ from braket.schema_common import BraketSchemaBase, BraketSchemaHeader
 # TODO: Replace the calibration data with actual values we receive from the device.
 
 
-GateFidelityType = TypeVar("GateFidelityType", bound=Dict[str, Union[str, float]])
-OneQubitType = TypeVar("OneQubitType", bound=Union[float, List[GateFidelityType]])
-TwoQubitType = TypeVar("TwoQubitType", bound=Dict[str, Union[float, Dict[str, int]]])
-
-QubitType = TypeVar("QubitType", bound=Dict[str, Union[OneQubitType, TwoQubitType]])
+OneQubitType = TypeVar("OneQubitType", bound=Dict[str, Union[float, int]])
+TwoQubitType = TypeVar("TwoQubitType", bound=Dict[str, Union[Dict[str, int], float]])
 
 
 class OqcProviderProperties(BraketSchemaBase):
@@ -32,8 +29,8 @@ class OqcProviderProperties(BraketSchemaBase):
     This defines the properties common to all the OQC devices.
 
     Attributes:
-        properties (Dict[str, Dict[str, Union[int, List[int]]]]): Basic specifications for
-            the device, such as gate fidelities and coherence times.
+        properties (Dict[str, Dict[str, Union[OneQubitType, TwoQubitType]]]): Basic
+            specifications for the device, such as gate fidelities and coherence times.
 
     Examples:
         >>> import json
@@ -45,23 +42,20 @@ class OqcProviderProperties(BraketSchemaBase):
         ... "properties": {
         ...     "one_qubit": {
         ...         "0": {
-        ...             "T1": 12.2,
-        ...             "T2": 13.5,
-        ...             "fRO": 0.99,
-        ...             "fRB": 0.98,
-        ...             "native-gate-fidelities": [
-        ...                 {"native-gate": "rz", "CLf": 0.99},
-        ...                 {"native-gate": "sx", "CLf": 0.99},
-        ...                 {"native-gate": "x", "CLf": 0.99},
-        ...             ],
-        ...             "EPE": 0.001,
+        ...             "T1": 28.9,
+        ...             "T2": 44.5,
+        ...             "fRB": 99.93,
+        ...             "fRO": 90.3,
+        ...             "qubit": 0
         ...         },
         ...     },
         ...     "two_qubit": {
         ...         "0-1": {
-        ...             "coupling": {"control_qubit": 0, "target_qubit": 1},
-        ...             "CLf": 0.99,
-        ...             "ECR_f": 0.99,
+        ...             "coupling": {
+        ...                 "control_qubit": 0,
+        ...                 "target_qubit": 1
+        ...             },
+        ...             "fCX": 87.7
         ...         },
         ...     },
         ... },
@@ -73,4 +67,4 @@ class OqcProviderProperties(BraketSchemaBase):
         name="braket.device_schema.oqc.oqc_provider_properties", version="1"
     )
     braketSchemaHeader: BraketSchemaHeader = Field(default=_PROGRAM_HEADER, const=_PROGRAM_HEADER)
-    properties: Dict[str, Dict[str, QubitType]]
+    properties: Dict[str, Dict[str, Union[OneQubitType, TwoQubitType]]]
