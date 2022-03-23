1319d8de55edc5733d5a1230d520b02c4ab94da7
Matt Cordoba <matcor@amazon.com>
2022-03-23 14:29:35 -0700

    Revert "fix: make provider properties specs to keep consistent with similar properties for other QHPs.  Make OneQubit gate number an integer instead of a float"
    This reverts commit 8599541d5962e89da05c6027f9ea64bafd01253a.

--- test/unit_tests/braket/device_schema/oqc/test_oqc_provider_properties_v1.py
+++ test/unit_tests/braket/device_schema/oqc/test_oqc_provider_properties_v1.py
@@ -26,49 +26,145 @@ def valid_input():
             "name": "braket.device_schema.oqc.oqc_provider_properties",
             "version": "1",
         },
-        "specs": {
+        "properties": {
             "one_qubit": {
-                "0": {"T1": 12.2, "T2": 13.5, "fRO": 0.99, "fRB": 0.98, "qubit": 0},
-                "1": {"T1": 12.2, "T2": 13.5, "fRO": 0.99, "fRB": 0.98, "qubit": 1},
-                "2": {"T1": 12.2, "T2": 13.5, "fRO": 0.99, "fRB": 0.98, "qubit": 2},
-                "3": {"T1": 12.2, "T2": 13.5, "fRO": 0.99, "fRB": 0.98, "qubit": 3},
-                "4": {"T1": 12.2, "T2": 13.5, "fRO": 0.99, "fRB": 0.98, "qubit": 4},
-                "5": {"T1": 12.2, "T2": 13.5, "fRO": 0.99, "fRB": 0.98, "qubit": 5},
-                "6": {"T1": 12.2, "T2": 13.5, "fRO": 0.99, "fRB": 0.98, "qubit": 6},
-                "7": {"T1": 12.2, "T2": 13.5, "fRO": 0.99, "fRB": 0.98, "qubit": 7},
+                "0": {
+                    "T1": 12.2,
+                    "T2": 13.5,
+                    "fRO": 0.99,
+                    "fRB": 0.98,
+                    "native_gate_fidelities": [
+                        {"native_gate": "rz", "CLf": 0.99},
+                        {"native_gate": "sx", "CLf": 0.99},
+                        {"native_gate": "x", "CLf": 0.99},
+                    ],
+                    "EPE": 0.001,
+                },
+                "1": {
+                    "T1": 12.2,
+                    "T2": 13.5,
+                    "fRO": 0.99,
+                    "fRB": 0.98,
+                    "native_gate_fidelities": [
+                        {"native_gate": "rz", "CLf": 0.99},
+                        {"native_gate": "sx", "CLf": 0.99},
+                        {"native_gate": "x", "CLf": 0.99},
+                    ],
+                    "EPE": 0.001,
+                },
+                "2": {
+                    "T1": 12.2,
+                    "T2": 13.5,
+                    "fRO": 0.99,
+                    "fRB": 0.98,
+                    "native_gate_fidelities": [
+                        {"native_gate": "rz", "CLf": 0.99},
+                        {"native_gate": "sx", "CLf": 0.99},
+                        {"native_gate": "x", "CLf": 0.99},
+                    ],
+                    "EPE": 0.001,
+                },
+                "3": {
+                    "T1": 12.2,
+                    "T2": 13.5,
+                    "fRO": 0.99,
+                    "fRB": 0.98,
+                    "native_gate_fidelities": [
+                        {"native_gate": "rz", "CLf": 0.99},
+                        {"native_gate": "sx", "CLf": 0.99},
+                        {"native_gate": "x", "CLf": 0.99},
+                    ],
+                    "EPE": 0.001,
+                },
+                "4": {
+                    "T1": 12.2,
+                    "T2": 13.5,
+                    "fRO": 0.99,
+                    "fRB": 0.98,
+                    "native_gate_fidelities": [
+                        {"native_gate": "rz", "CLf": 0.99},
+                        {"native_gate": "sx", "CLf": 0.99},
+                        {"native_gate": "x", "CLf": 0.99},
+                    ],
+                    "EPE": 0.001,
+                },
+                "5": {
+                    "T1": 12.2,
+                    "T2": 13.5,
+                    "fRO": 0.99,
+                    "fRB": 0.98,
+                    "native_gate_fidelities": [
+                        {"native_gate": "rz", "CLf": 0.99},
+                        {"native_gate": "sx", "CLf": 0.99},
+                        {"native_gate": "x", "CLf": 0.99},
+                    ],
+                    "EPE": 0.001,
+                },
+                "6": {
+                    "T1": 12.2,
+                    "T2": 13.5,
+                    "fRO": 0.99,
+                    "fRB": 0.98,
+                    "native_gate_fidelities": [
+                        {"native_gate": "rz", "CLf": 0.99},
+                        {"native_gate": "sx", "CLf": 0.99},
+                        {"native_gate": "x", "CLf": 0.99},
+                    ],
+                    "EPE": 0.001,
+                },
+                "7": {
+                    "T1": 12.2,
+                    "T2": 13.5,
+                    "fRO": 0.99,
+                    "fRB": 0.98,
+                    "native_gate_fidelities": [
+                        {"native_gate": "rz", "CLf": 0.99},
+                        {"native_gate": "sx", "CLf": 0.99},
+                        {"native_gate": "x", "CLf": 0.99},
+                    ],
+                    "EPE": 0.001,
+                },
             },
             "two_qubit": {
                 "0-1": {
                     "coupling": {"control_qubit": 0, "target_qubit": 1},
-                    "fCX": 0.99,
+                    "CLf": 0.99,
+                    "ECR_f": 0.99,
                 },
                 "1-2": {
                     "coupling": {"control_qubit": 1, "target_qubit": 2},
-                    "fCX": 0.99,
+                    "CLf": 0.99,
+                    "ECR_f": 0.99,
                 },
                 "2-3": {
                     "coupling": {"control_qubit": 2, "target_qubit": 3},
-                    "fCX": 0.99,
+                    "CLf": 0.99,
+                    "ECR_f": 0.99,
                 },
                 "3-4": {
                     "coupling": {"control_qubit": 3, "target_qubit": 4},
-                    "fCX": 0.99,
+                    "CLf": 0.99,
+                    "ECR_f": 0.99,
                 },
                 "4-5": {
                     "coupling": {"control_qubit": 4, "target_qubit": 5},
-                    "fCX": 0.99,
+                    "CLf": 0.99,
+                    "ECR_f": 0.99,
                 },
                 "5-6": {
                     "coupling": {"control_qubit": 5, "target_qubit": 6},
-                    "fCX": 0.99,
+                    "CLf": 0.99,
+                    "ECR_f": 0.99,
                 },
                 "6-7": {
                     "coupling": {"control_qubit": 6, "target_qubit": 7},
-                    "fCX": 0.99,
+                    "CLf": 0.99,
+                    "ECR_f": 0.99,
                 },
                 "7-0": {
                     "coupling": {"control_qubit": 7, "target_qubit": 0},
-                    "fCX": 0.99,
+                    "CLf": 0.99,
+                    "ECR_f": 0.99,
                 },
             },
         },
@@ -81,7 +177,7 @@ def test_valid(valid_input):
     assert result.braketSchemaHeader.name == "braket.device_schema.oqc.oqc_provider_properties"
 
 
-@pytest.mark.parametrize("missing_field", ["braketSchemaHeader", "specs"])
+@pytest.mark.parametrize("missing_field", ["braketSchemaHeader", "properties"])
 def test_missing_field(valid_input, missing_field):
     with pytest.raises(ValidationError):
         valid_input.pop(missing_field)
