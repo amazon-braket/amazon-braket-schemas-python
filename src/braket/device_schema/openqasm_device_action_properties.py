845804b999c88c047ebab0b05809b4306f066d5a
Aaron Berdy <berdy@amazon.com>
2023-04-24 13:39:06 -0700

    lint

--- src/braket/device_schema/openqasm_device_action_properties.py
+++ src/braket/device_schema/openqasm_device_action_properties.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from typing import List, Optional, Type, Union
+from typing import List, Optional, Union
 
 from pydantic import BaseModel, constr
 
