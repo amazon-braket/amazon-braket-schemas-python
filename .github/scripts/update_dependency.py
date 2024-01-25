3377932c5200fc5fde5b1d4811fceffdce782d99
Abe Coull <85974725+math411@users.noreply.github.com>
2024-01-25 14:51:52 -0800

    Update update_dependency.py

--- .github/scripts/update_dependency.py
+++ .github/scripts/update_dependency.py
@@ -43,7 +43,7 @@ for line in fileinput.input("setup.py", inplace=True):
 
 
 for line in fileinput.input("tox.ini", inplace=True):
-    # Ensure that tox uses the working branch for the SDK PR.
+    # Ensure that tox uses the working branch for the schema PR.
     replaced_line = (
         line if package not in line else f'"{package} @ file://{path}/{package}-python",\n'
     )
