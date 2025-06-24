d32bd90089593b969074a87dec9ee5f5a2170136
Sai <138810078+sai-sp3@users.noreply.github.com>
2025-06-24 09:37:02 -0700

    fix: updating IonQ device capability schema to include standardized property (#196)

--- test/unit_tests/braket/device_schema/ionq/test_ionq_device_capabilities_v1.py
+++ test/unit_tests/braket/device_schema/ionq/test_ionq_device_capabilities_v1.py
@@ -159,6 +159,64 @@ def test_valid_provider(valid_input):
     assert result == IonqDeviceCapabilities.parse_raw(result.json())
 
 
+def test_valid_standardized():
+    valid_input = dict(openqasm_valid_input)
+
+    valid_input["standardized"] = {
+        "braketSchemaHeader": {
+            "name": "braket.device_schema.standardized_gate_model_qpu_device_properties",
+            "version": "3",
+        },
+        "oneQubitProperties": {
+            "0": {
+                "oneQubitFidelity": [
+                    {
+                        "fidelityType": {
+                            "name": "RANDOMIZED_BENCHMARKING",
+                            "description": "Single qubit randomized benchmarking",
+                        },
+                        "fidelity": 0.9985,
+                        "standardError": 0.0003,
+                        "unit": "fraction",
+                    }
+                ]
+            },
+            "1": {
+                "oneQubitFidelity": [
+                    {
+                        "fidelityType": {
+                            "name": "RANDOMIZED_BENCHMARKING",
+                            "description": "Single qubit randomized benchmarking",
+                        },
+                        "fidelity": 0.9982,
+                        "standardError": 0.0004,
+                        "unit": "fraction",
+                    }
+                ]
+            },
+        },
+        "T1": {"value": 50.0, "standardError": 2.5, "unit": "s"},
+        "T2": {"value": 30.0, "standardError": 1.5, "unit": "s"},
+        "readoutFidelity": [{"fidelity": 0.9950, "standardError": 0.0010, "unit": "fraction"}],
+        "readoutDuration": {"value": 0.000350, "standardError": 0.000010, "unit": "s"},
+        "singleQubitGateDuration": {"value": 0.000020, "standardError": 0.000002, "unit": "s"},
+        "twoQubitGateFidelity": [
+            {
+                "fidelityType": {
+                    "name": "RANDOMIZED_BENCHMARKING",
+                    "description": "Two qubit randomized benchmarking",
+                },
+                "fidelity": 0.9950,
+                "standardError": 0.0010,
+                "unit": "fraction",
+            }
+        ],
+        "twoQubitGateDuration": {"value": 0.000200, "standardError": 0.000010, "unit": "s"},
+        "updatedAt": "2025-02-22T12:29:03Z",
+    }
+    IonqDeviceCapabilities.parse_raw_schema(json.dumps(valid_input))
+
+
 @pytest.mark.parametrize("valid_input", [openqasm_valid_input, jaqcd_valid_input])
 @pytest.mark.xfail(raises=ValidationError)
 def test__missing_schemaHeader(valid_input):
